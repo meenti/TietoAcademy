11bfa38c5120afd7a2f7d46bdf590bdf0bbe3402
Meenti <you@example.com>
2019-07-04 18:26:29 +0200

    Added fibonacci sequence in counter section

--- ClientApp/src/app/counter/counter.component.ts
+++ ClientApp/src/app/counter/counter.component.ts
@@ -6,8 +6,22 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   public currentCount = 0;
+  public fibonacciNumber = 1;
+  public nextFibonacciNnumber = 0;
+  public click = 1;
+  public i;
+
 
   public incrementCounter() {
-    this.currentCount = this.currentCount +5;
+    //this.currentCount = this.currentCount +5;
+   
+    for ( this.i = 0; this.i < this.click ; this.i++)
+    {
+      this.currentCount = this.fibonacciNumber;
+      this.fibonacciNumber = this.fibonacciNumber + this.nextFibonacciNnumber;
+      this.nextFibonacciNnumber = this.fibonacciNumber - this.nextFibonacciNnumber ;
+    }
+    this.click = this.click++;
+
   }
 }
