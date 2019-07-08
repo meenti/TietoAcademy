3051c9c541e0bb100eb67bf579ed94649d918bac
meenti <wiczkawala21@gmail.com>
2019-07-08 11:33:32 +0200

    Feature 9 -  Unit test
    In this case have been tested conversion Celsius to Fahrenheit

---
+++ toFahrenheit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("toFahrenheit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("toFahrenheit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8bc89d7f-5d7a-459e-9a51-92ca5a35783f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ toFahrenheit/toFahrenheit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApplication;
+
+
+namespace toFahrenheit
+{
+    [TestClass]
+    public class toFahrenheit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WeatherForecast temp = new WeatherForecast
+            {
+                TemperatureC = 20
+            };
+            int expected = 68;
+            temp.TemperatureF = 32 + (int)(temp.TemperatureC / 0.5556);
+            Assert.AreEqual(expected, temp.TemperatureF);
+        }
+    }
+}
